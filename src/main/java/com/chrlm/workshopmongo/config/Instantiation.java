6614ef487afe030924280bb12618be43b8cd996a
Carlos Racobaldo Montes <carloshracobaldo@gmail.com>
2026-04-07 11:53:13 -0300

    Feat class AuthorDTO and reference between User and Posts

--- src/main/java/com/chrlm/workshopmongo/config/Instantiation.java
+++ src/main/java/com/chrlm/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.chrlm.workshopmongo.domain.Post;
 import com.chrlm.workshopmongo.domain.User;
+import com.chrlm.workshopmongo.dto.AuthorDTO;
 import com.chrlm.workshopmongo.repository.PostRepository;
 import com.chrlm.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,16 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou pra SP", maria );
-		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hj", maria );
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou pra SP", new AuthorDTO(maria) );
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hj", new AuthorDTO(maria) );
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
