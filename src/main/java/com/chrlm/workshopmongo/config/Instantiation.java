f982f811c5aee0448467fc5f58efa6674e192ed8
Carlos Racobaldo Montes <carloshracobaldo@gmail.com>
2026-04-07 13:54:06 -0300

    Feat CommentDTO

--- src/main/java/com/chrlm/workshopmongo/config/Instantiation.java
+++ src/main/java/com/chrlm/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.chrlm.workshopmongo.domain.Post;
 import com.chrlm.workshopmongo.domain.User;
 import com.chrlm.workshopmongo.dto.AuthorDTO;
+import com.chrlm.workshopmongo.dto.CommentDTO;
 import com.chrlm.workshopmongo.repository.PostRepository;
 import com.chrlm.workshopmongo.repository.UserRepository;
 
@@ -41,6 +42,12 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou pra SP", new AuthorDTO(maria) );
 		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hj", new AuthorDTO(maria) );
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Se divirta", sdf.parse("21/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Bom dia", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
